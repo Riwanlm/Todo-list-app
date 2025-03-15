5734c7426e4975fbbeb1e8cf79e04ac55c70521b
Riwanlm <riwanlm@gmail.com>
2025-03-15 14:07:45 +0100

    ajout de la sauvegarde des todos dans le localStorage, et modification du add, update et delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Plus, Trash2 } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export type Todo = {
   id: number;
@@ -31,6 +31,30 @@ export default function App() {
     },
   ]);
 
+  //Récupère les todos du localStorage et les ajoute au state
+  useEffect(() => {
+    const newTodos: Todo[] = [];
+
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      if (key && !isNaN(Number(key))) {
+        const todoStorage: Todo = JSON.parse(
+          localStorage.getItem(key.toString())!
+        );
+        newTodos.push(todoStorage);
+      }
+    }
+
+    // Utilisation d'un Map pour éviter les doublons
+    const uniqueTodos = [
+      ...new Map(
+        [...todos, ...newTodos].map((todo) => [todo.id, todo])
+      ).values(),
+    ];
+
+    setTodos(uniqueTodos);
+  }, []);
+
   const addTodo = (value: string) => {
     if (value.length === 0) {
       return;
@@ -40,21 +64,31 @@ export default function App() {
       name: value,
       isChecked: false,
     };
+    localStorage.setItem(newTodo.id.toString(), JSON.stringify(newTodo));
     setTodos([...todos, newTodo]);
   };
 
-  const updateTodo = (id: number, updatedTodo: Todo) => {
+  const updateTodo = (id: number, newTodo: Todo) => {
     const newTodos = todos.map((todo) => {
       if (todo.id !== id) {
         return todo;
       }
-      return updatedTodo;
+      const updateTodoInLocalStorage = localStorage.getItem(id.toString());
+      if (updateTodoInLocalStorage) {
+        localStorage.removeItem(id.toString());
+        localStorage.setItem(id.toString(), JSON.stringify(newTodo));
+      }
+      return newTodo;
     });
     setTodos(newTodos);
   };
 
   const deleteTodo = (deleteTodo: Todo) => {
     const newTodos = todos.filter((todo) => todo.id !== deleteTodo.id);
+    const deletedTodoInStorage = localStorage.getItem(deleteTodo.id.toString());
+    if (deletedTodoInStorage) {
+      localStorage.removeItem(deleteTodo.id.toString());
+    }
     setTodos(newTodos);
   };
 
@@ -93,52 +127,56 @@ export default function App() {
           </fieldset>
           <div className="divider divider-accent">Liste</div>
           <ul className="list-none mt-2">
-            {todos.length !== 0
-              ? todos.map((todo) => (
-                  <li
-                    key={todo.id}
-                    className="flex items-center gap-2 mb-2 bg-gray-600 rounded-lg p-1"
+            {todos.length !== 0 ? (
+              todos.map((todo) => (
+                <li
+                  key={todo.id}
+                  className="flex items-center gap-2 mb-2 bg-gray-600 rounded-lg p-1"
+                >
+                  <input
+                    className="checkbox checkbox-sm "
+                    type="checkbox"
+                    checked={todo.isChecked}
+                    onChange={() => {
+                      const newChecked = !todo.isChecked;
+                      updateTodo(todo.id, {
+                        ...todo,
+                        isChecked: newChecked,
+                      });
+                    }}
+                  />
+                  <input
+                    type="text"
+                    defaultValue={todo.name}
+                    disabled={todo.isChecked}
+                    className={
+                      todo.isChecked
+                        ? "line-through text-green-200"
+                        : " text-red-200"
+                    }
+                    onBlur={(e) => {
+                      const newValue = e.target.value;
+                      updateTodo(todo.id, {
+                        ...todo,
+                        name: newValue,
+                      });
+                    }}
+                  />
+                  <button
+                    className="btn btn-accent ml-3 rounded-lg"
+                    onClick={() => {
+                      deleteTodo(todo);
+                    }}
                   >
-                    <input
-                      className="checkbox checkbox-sm "
-                      type="checkbox"
-                      checked={todo.isChecked}
-                      onChange={() => {
-                        const newChecked = !todo.isChecked;
-                        updateTodo(todo.id, {
-                          ...todo,
-                          isChecked: newChecked,
-                        });
-                      }}
-                    />
-                    <input
-                      type="text"
-                      defaultValue={todo.name}
-                      disabled={todo.isChecked}
-                      className={
-                        todo.isChecked
-                          ? "line-through text-green-200"
-                          : " text-red-200"
-                      }
-                      onBlur={(e) => {
-                        const newValue = e.target.value;
-                        updateTodo(todo.id, {
-                          ...todo,
-                          name: newValue,
-                        });
-                      }}
-                    />
-                    <button
-                      className="btn btn-accent ml-3 rounded-lg"
-                      onClick={() => {
-                        deleteTodo(todo);
-                      }}
-                    >
-                      <Trash2 size={20} className="text-white" />
-                    </button>
-                  </li>
-                ))
-              : null}
+                    <Trash2 size={20} className="text-white" />
+                  </button>
+                </li>
+              ))
+            ) : (
+              <p className="text-accent">
+                Ajoutez de nouveaux todos à votre liste !
+              </p>
+            )}
           </ul>
         </div>
       </div>
